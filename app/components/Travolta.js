0c504b6b8907feefc6b679756de7b3d5831dd8ac
JoaoCnh <joaomarinho.cunha@gmail.com>
2016-11-24 17:10:46 +0000

    [app] Added styling and refactor of 404 page

--- app/components/Travolta.js
+++ app/components/Travolta.js
@@ -1,17 +1,19 @@
 import React from 'react';
 
 class Travolta extends React.Component {
-  render() {
-    return (
-      <div>
-        <div className="background"></div>
-        <div className="message">
-          <h1>404</h1>
-          <h2>{this.props.msg || 'There\'s Nothing Here'}</h2>
-        </div>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <div className="travolta-background"></div>
+                <div className="travolta-message">
+                    <h1 className="travolta-header">404</h1>
+                    <h2 className="travolta-sub-header">
+                        {this.props.msg || 'There\'s Nothing Here'}
+                    </h2>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default Travolta;
--- public/app/bundle.js
+++ public/app/bundle.js
@@ -29534,7 +29534,7 @@
 	"use strict";
 
 	Object.defineProperty(exports, "__esModule", {
-	  value: true
+	    value: true
 	});
 
 	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
@@ -29552,40 +29552,40 @@
 	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
 	var Travolta = function (_React$Component) {
-	  _inherits(Travolta, _React$Component);
+	    _inherits(Travolta, _React$Component);
 
-	  function Travolta() {
-	    _classCallCheck(this, Travolta);
+	    function Travolta() {
+	        _classCallCheck(this, Travolta);
 
-	    return _possibleConstructorReturn(this, (Travolta.__proto__ || Object.getPrototypeOf(Travolta)).apply(this, arguments));
-	  }
-
-	  _createClass(Travolta, [{
-	    key: "render",
-	    value: function render() {
-	      return _react2.default.createElement(
-	        "div",
-	        null,
-	        _react2.default.createElement("div", { className: "background" }),
-	        _react2.default.createElement(
-	          "div",
-	          { className: "message" },
-	          _react2.default.createElement(
-	            "h1",
-	            null,
-	            "404"
-	          ),
-	          _react2.default.createElement(
-	            "h2",
-	            null,
-	            this.props.msg || 'There\'s Nothing Here'
-	          )
-	        )
-	      );
+	        return _possibleConstructorReturn(this, (Travolta.__proto__ || Object.getPrototypeOf(Travolta)).apply(this, arguments));
 	    }
-	  }]);
 
-	  return Travolta;
+	    _createClass(Travolta, [{
+	        key: "render",
+	        value: function render() {
+	            return _react2.default.createElement(
+	                "div",
+	                null,
+	                _react2.default.createElement("div", { className: "travolta-background" }),
+	                _react2.default.createElement(
+	                    "div",
+	                    { className: "travolta-message" },
+	                    _react2.default.createElement(
+	                        "h1",
+	                        { className: "travolta-header" },
+	                        "404"
+	                    ),
+	                    _react2.default.createElement(
+	                        "h2",
+	                        { className: "travolta-sub-header" },
+	                        this.props.msg || 'There\'s Nothing Here'
+	                    )
+	                )
+	            );
+	        }
+	    }]);
+
+	    return Travolta;
 	}(_react2.default.Component);
 
 	exports.default = Travolta;
